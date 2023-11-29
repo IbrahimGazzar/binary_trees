d138394629e6a4707fb4c3539907391853868f3c
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-29 04:41:20 -0800

    Use a helper function to measure the height of root's children

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,30 +1,37 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height_2 - measures the height of a binary tree
+ * @tree: pointer to the root of the tree to be measured
+ *
+ * Return: height of the tree
+ */
+
+int binary_tree_height_2(const binary_tree_t *tree)
+{
+	size_t height_l = 1, height_r = 1;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		height_l = 1 + binary_tree_height_2(tree->left);
+	if (tree->right)
+		height_r = 1 + binary_tree_height_2(tree->right);
+	if (height_l > height_r)
+		return (height_l);
+	else
+		return (height_r);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a tree
  * @tree: pointer to the root node of the tree to be checked
  *
  * Return: balance factor
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int b_left = 0, b_right = 0;
-	binary_tree_t *ptr_left, *ptr_right;
-
 	if (tree == NULL)
 		return (0);
-	ptr_left = tree->left;
-	while (ptr_left != NULL)
-	{
-		b_left++;
-		ptr_left = ptr_left->left;
-	}
-	ptr_right = tree->right;
-	while (ptr_right != NULL)
-	{
-		b_right++;
-		ptr_right = ptr_right->right;
-	}
-	return (b_left - b_right);
+	return (binary_tree_height_2(tree->left) - binary_tree_height_2(tree->right));
 }
